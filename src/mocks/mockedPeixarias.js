cdda5bfcb351bd44606317f26db798ac3d0cb057
lmattoos <lorenzo13mattos@gmail.com>
2026-08-16 20:00:11 -0300

    feat: mocks para exemplo do projeto

---
+++ src/mocks/mockedPeixarias.js
@@ -0,0 +1,68 @@
+export const mockedPeixarias = [
+  {
+    id: 1,
+    nome: "Peixaria Mar Aberto",
+    foto: "./public/img/Shoal(Logo).png",
+    cep: "12345-678",
+    cnpj: "11.222.333/0001-44",
+    telefone: "(11) 98765-4321",
+    email: "contato@maraberto.com",
+    descricao:
+      "Especializada em frutos do mar frescos e de alta qualidade, trazidos diretamente da costa todos os dias.",
+  },
+  {
+    id: 2,
+    nome: "O Cantinho do Pescador",
+    foto: "./public/img/Shoal(Logo).png",
+    cep: "54321-876",
+    cnpj: "22.333.444/0001-55",
+    telefone: "(21) 91234-5678",
+    email: "vendas@cantinhodopescador.com",
+    descricao:
+      "Ambiente acolhedor e familiar, com grande variedade de peixes de rio e mar, além de pratos prontos para levar.",
+  },
+  {
+    id: 3,
+    nome: "Delícias do Oceano",
+    foto: "./public/img/Shoal(Logo).png",
+    cep: "98765-432",
+    cnpj: "33.444.555/0001-66",
+    telefone: "(31) 99887-7665",
+    email: "deliciasdooceano@email.com",
+    descricao:
+      "Fornecedora de pescados exóticos e raros. Ideal para chefs de cozinha e restaurantes que buscam ingredientes diferenciados.",
+  },
+  {
+    id: 4,
+    nome: "Porto Seguro Pescados",
+    foto: "./public/img/Shoal(Logo).png",
+    cep: "67890-123",
+    cnpj: "44.555.666/0001-77",
+    telefone: "(41) 97654-3210",
+    email: "sac@portoseguro.com",
+    descricao:
+      "Peixaria com foco em sustentabilidade e rastreabilidade. Todos os produtos são provenientes de pesca certificada e responsável.",
+  },
+  {
+    id: 5,
+    nome: "Rei do Salmão",
+    foto: "./public/img/Shoal(Logo).png",
+    cep: "32109-876",
+    cnpj: "55.666.777/0001-88",
+    telefone: "(51) 96543-2109",
+    email: "contato@reidosalmao.com.br",
+    descricao:
+      "Especialista em salmão fresco e defumado, além de outros peixes nobres como o bacalhau do Alasca e atum.",
+  },
+  {
+    id: 6,
+    nome: "Rei do Salmão",
+    foto: "./public/img/Shoal(Logo).png",
+    cep: "32109-876",
+    cnpj: "55.666.777/0001-88",
+    telefone: "(51) 96543-2109",
+    email: "contato@reidosalmao.com.br",
+    descricao:
+      "Especialista em salmão fresco e defumado, além de outros peixes nobres como o bacalhau do Alasca e atum.",
+  },
+];
